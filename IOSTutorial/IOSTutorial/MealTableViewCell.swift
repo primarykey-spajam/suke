322b1e68585965c00b713b9d9b6ffdaceaff5a3b
sk-mk2 <suke.badminton7@icloud.com>
2019-04-29 01:55:43 +0900

    IOS tutorial テーブルビューを作成まで

---
+++ IOSTutorial/IOSTutorial/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  IOSTutorial
+//
+//  Created by 助永悠輝 on 2019/04/29.
+//  Copyright © 2019 助永悠輝. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- IOSTutorial/IOSTutorial/MealViewController.swift
+++ IOSTutorial/IOSTutorial/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  IOSTutorial
 //
 //  Created by 助永悠輝 on 2019/04/28.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController,
+class MealViewController: UIViewController,
 UITextFieldDelegate,
 UINavigationControllerDelegate,
 UIImagePickerControllerDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     override func viewDidLoad() {
@@ -31,7 +30,6 @@ UIImagePickerControllerDelegate {
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
     }
     
     //MARK: Actions
