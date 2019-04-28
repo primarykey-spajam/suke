09f8eee389bb02c23a9ee1be7df5a13f7480e899
sk-mk2 <suke.badminton7@icloud.com>
2019-04-28 09:08:59 +0900

    feat: テーブル表示成功

--- test/SecondViewController.swift
+++ test/SecondViewController.swift
@@ -1,33 +1,38 @@
 //
-//  SecondViewController.swift
+//  ViewController.swift
 //  test
 //
-//  Created by 助永悠輝 on 2019/04/27.
+//  Created by 助永悠輝 on 2019/04/24.
 //  Copyright © 2019 助永悠輝. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
-    @IBAction func swipeHandler(_ sender: Any) {
-        let nextView = storyboard!.instantiateViewController(withIdentifier: "Third")
-        self.present(nextView, animated: true, completion: nil)
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let fruits = ["リンゴ", "みかん", "ぶどう"]
+    /// セルの個数を指定するデリゲートメソッド（必須）
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count;
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /// セルに値を設定するデータソースメソッド（必須）
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        // セルに表示する値を設定する
+        cell.textLabel!.text = fruits[indexPath.row]
+        return cell
     }
-    */
-
+    
+    /// セルが選択された時に呼ばれるデリゲートメソッド
+    private func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("セル番号：(indexPath.row) セルの内容：(fruits[indexPath.row])")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
 }
+
